9a082ebfc6e5cbaf223b4df844410727949791db
ChristianGinko <christianginko@gmail.com>
2025-10-06 18:04:32 -0300

    Sumar tablas

--- src/main/java/com/chris/pokedex/model/Pokeapi.java
+++ src/main/java/com/chris/pokedex/model/Pokeapi.java
@@ -2,13 +2,60 @@ package com.chris.pokedex.model;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name = "pokemon")
 public class Pokeapi {
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String pokemon;
+
+    private String nombre;
+
+    // Relación con Tipo
+    @ManyToMany
+    @JoinTable(
+            name = "pokemon_tipo",
+            joinColumns = @JoinColumn(name = "pokemon_id"),
+            inverseJoinColumns = @JoinColumn(name = "tipo_id")
+    )
+    private List<Tipo> tipo;
+
+    // Relación con Habilidad (un Pokémon puede tener varias habilidades)
+    @ManyToMany
+    @JoinTable(
+            name = "pokemon_habilidad", // tabla intermedia
+            joinColumns = @JoinColumn(name = "pokemon_id"),
+            inverseJoinColumns = @JoinColumn(name = "habilidad_id")
+    )
+    private List<Habilidades> habilidades;
+
+    // Getters y setters
+
+    public List<Habilidades> getHabilidades() {
+        return habilidades;
+    }
+
+    public void setHabilidades(List<Habilidades> habilidades) {
+        this.habilidades = habilidades;
+    }
+
+    public List<Tipo> getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(List<Tipo> tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
     public Long getId() {
         return id;
@@ -16,13 +63,5 @@ public class Pokeapi {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getPokemon() {
-        return pokemon;
-    }
-
-    public void setPokemon(String pokemon) {
-        this.pokemon = pokemon;
     }
 }
