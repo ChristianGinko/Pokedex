8a8ac59668de9e72ac1f9f42c9e87e75a1baa985
ChristianGinko <christianginko@gmail.com>
2025-10-09 17:18:39 -0300

    Cambios

--- src/main/java/com/chris/pokedex/controller/TipoController.java
+++ src/main/java/com/chris/pokedex/controller/TipoController.java
@@ -3,9 +3,11 @@ package com.chris.pokedex.controller;
 import com.chris.pokedex.model.Tipo;
 import com.chris.pokedex.repository.TipoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/tipo")
@@ -21,5 +23,47 @@ public class TipoController {
     @GetMapping
     public List<Tipo> listarTipos() {
         return tipoRepository.findAll();
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Tipo> actualizarTipo(@PathVariable Long id, @RequestBody Tipo datosActualizados) {
+        Optional<Tipo> tipoOpt = tipoRepository.findById(id);
+        if (tipoOpt.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Tipo tipo = tipoOpt.get();
+
+        // Si se manda un nuevo nombre
+        if (datosActualizados.getNombre() != null) {
+            tipo.setNombre(datosActualizados.getNombre());
+        }
+
+        if (datosActualizados.getDobleDanoDe() != null && !datosActualizados.getDobleDanoDe().isEmpty()) {
+            tipo.getDobleDanoDe().addAll(datosActualizados.getDobleDanoDe());
+        }
+
+        if (datosActualizados.getDobleDanoA() != null && !datosActualizados.getDobleDanoA().isEmpty()) {
+            tipo.getDobleDanoA().addAll(datosActualizados.getDobleDanoA());
+        }
+
+        if (datosActualizados.getMitadDanoDe() != null && !datosActualizados.getMitadDanoDe().isEmpty()) {
+            tipo.getMitadDanoDe().addAll(datosActualizados.getMitadDanoDe());
+        }
+
+        if (datosActualizados.getMitadDanoA() != null && !datosActualizados.getMitadDanoA().isEmpty()) {
+            tipo.getMitadDanoA().addAll(datosActualizados.getMitadDanoA());
+        }
+
+        if (datosActualizados.getSinDanoDe() != null && !datosActualizados.getSinDanoDe().isEmpty()) {
+            tipo.getSinDanoDe().addAll(datosActualizados.getSinDanoDe());
+        }
+
+        if (datosActualizados.getSinDanoA() != null && !datosActualizados.getSinDanoA().isEmpty()) {
+            tipo.getSinDanoA().addAll(datosActualizados.getSinDanoA());
+        }
+
+        Tipo actualizado = tipoRepository.save(tipo);
+        return ResponseEntity.ok(actualizado);
     }
 }
