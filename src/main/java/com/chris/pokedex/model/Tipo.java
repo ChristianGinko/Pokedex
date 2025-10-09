fa187c7e84284f2c206a46c3833b803f41c0fb24
ChristianGinko <christianginko@gmail.com>
2025-10-09 16:30:23 -0300

    Cambios

--- src/main/java/com/chris/pokedex/model/Tipo.java
+++ src/main/java/com/chris/pokedex/model/Tipo.java
@@ -16,19 +16,51 @@ public class Tipo {
 
     @ManyToMany
     @JoinTable(
-            name = "doble_daño_de",
+            name = "doble_dano_de",
             joinColumns = @JoinColumn(name = "tipo_id"),
-            inverseJoinColumns = @JoinColumn(name = "doble_daño_de_id")
+            inverseJoinColumns = @JoinColumn(name = "doble_dano_de_id")
     )
-    private Set<Tipo> dobleDañoDe = new HashSet<>();
+    private Set<Tipo> dobleDanoDe = new HashSet<>();
 
     @ManyToMany
     @JoinTable(
-            name = "doble_daño_a",
+            name = "doble_dano_a",
             joinColumns = @JoinColumn(name = "tipo_id"),
-            inverseJoinColumns = @JoinColumn(name = "doble_daño_a_id")
+            inverseJoinColumns = @JoinColumn(name = "doble_dano_a_id")
     )
-    private Set<Tipo> dobleDañoA = new HashSet<>();
+    private Set<Tipo> dobleDanoA = new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "mitad_dano_de",
+            joinColumns = @JoinColumn(name = "tipo_id"),
+            inverseJoinColumns = @JoinColumn(name = "mitad_dano_de_id")
+    )
+    private Set<Tipo> mitadDanoDe = new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "mitad_dano_a",
+            joinColumns = @JoinColumn(name = "tipo_id"),
+            inverseJoinColumns = @JoinColumn(name = "mitad_dano_a_id")
+    )
+    private Set<Tipo> mitadDanoA = new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "sin_dano_de",
+            joinColumns = @JoinColumn(name = "tipo_id"),
+            inverseJoinColumns = @JoinColumn(name = "sin_dano_de_id")
+    )
+    private Set<Tipo> sinDanoDe = new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "sin_dano_a",
+            joinColumns = @JoinColumn(name = "tipo_id"),
+            inverseJoinColumns = @JoinColumn(name = "sin_dano_a_id")
+    )
+    private Set<Tipo> sinDanoA = new HashSet<>();
 
     // Constructor, getters y setters
     public Tipo() {}
@@ -51,5 +83,53 @@ public class Tipo {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public Set<Tipo> getDobleDanoDe() {
+        return dobleDanoDe;
+    }
+
+    public void setDobleDanoDe(Set<Tipo> dobleDanoDe) {
+        this.dobleDanoDe = dobleDanoDe;
+    }
+
+    public Set<Tipo> getMitadDanoDe() {
+        return mitadDanoDe;
+    }
+
+    public void setMitadDanoDe(Set<Tipo> mitadDanoDe) {
+        this.mitadDanoDe = mitadDanoDe;
+    }
+
+    public Set<Tipo> getDobleDanoA() {
+        return dobleDanoA;
+    }
+
+    public void setDobleDanoA(Set<Tipo> dobleDanoA) {
+        this.dobleDanoA = dobleDanoA;
+    }
+
+    public Set<Tipo> getMitadDanoA() {
+        return mitadDanoA;
+    }
+
+    public void setMitadDanoA(Set<Tipo> mitadDanoA) {
+        this.mitadDanoA = mitadDanoA;
+    }
+
+    public Set<Tipo> getSinDanoDe() {
+        return sinDanoDe;
+    }
+
+    public void setSinDanoDe(Set<Tipo> sinDanoDe) {
+        this.sinDanoDe = sinDanoDe;
+    }
+
+    public Set<Tipo> getSinDanoA() {
+        return sinDanoA;
+    }
+
+    public void setSinDanoA(Set<Tipo> sinDanoA) {
+        this.sinDanoA = sinDanoA;
     }
 }
